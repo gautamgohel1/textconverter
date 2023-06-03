fe8e7e0d55149547a86713238aba63253e1a816d
gautam <gohelgautam10027@gmail.com>
2023-06-03 15:13:26 +0530

    add sad emojis

--- src/topheder.js
+++ src/topheder.js
@@ -54,7 +54,7 @@ function Topheder(props) {
   };
   const handalclickcopy = (event) => {
     if (tex1 === "") {
-      setText2("Field Is Empty 😟");
+      setText2("Field Is Empty 😟😟");
     } else {
       navigator.clipboard.writeText(tex1);
       setText2("Copied");
